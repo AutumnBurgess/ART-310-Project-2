8a31652db399bf4b4d8a7a17640b53d1909bd853
AutumnBurgess <aus2001@live.missouristate.edu>
2024-02-22 11:57:54 -0600

    misc tinkering with test data
    nothing to do with the final project

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let dataTable;
+
+function preload() {
+  dataTable = loadTable("data.csv", "csv", "header");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  colorMode("HSB", 100);
 }
 
 function draw() {
   background(220);
+  for (let rowNumber = 0; rowNumber < dataTable.getRowCount(); rowNumber++) {
+    let row = dataTable.getRow(rowNumber);
+    let x = 100;
+    let y = rowNumber * 100 + 100;
+    let hue = row.getNum("HUE");
+    let c = color(hue, 100, 100);
+    fill(c);
+    ellipse(x, y, row.getNum("SIZE"));
+
+    text(row.getString("NAME"), x + 50, y);
+  }
 }
