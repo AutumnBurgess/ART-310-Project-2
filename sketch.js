464a5f26f7731b6e781d6dc1c77e6ad981a72e2f
AutumnBurgess <aus2001@live.missouristate.edu>
2024-03-07 12:43:27 -0600

    refactor clean example data

--- sketch.js
+++ sketch.js
@@ -17,40 +17,39 @@ function setup() {
 
 function draw() {
   background(220);
+  dataTable.rows.forEach(drawSpot);
+}
 
-  for (let dataRow of dataTable.rows){
-    const index = dataRow.getNum("INDEX");
-    const row = dataRow.getNum("ROW");
-    const color = dataRow.getString("COLOR");
-    const has_car = color.length > 0;
-    const grassy = dataRow.getNum("GRASSY") != 0;
-    const four_door = dataRow.getNum("FOUR_DOOR") != 0;
-    const position = getSpotPosition(row, index);
-    const x = position.x;
-    const y = position.y;
+function drawSpot(dataRow){
+  const index = dataRow.getNum("INDEX");
+  const row = dataRow.getNum("ROW");
+  const color = dataRow.getString("COLOR");
+  const has_car = color.length > 0;
+  const grassy = dataRow.getNum("GRASSY") != 0;
+  const four_door = dataRow.getNum("FOUR_DOOR") != 0;
+  const position = getSpotPosition(row, index);
+  const x = position.x;
+  const y = position.y;
 
-    //car
-    if (has_car){
-      fill(color);
-      noStroke();
-      const border = 5;
-      const height = four_door ? SPOT_HEIGHT : SPOT_HEIGHT * (5/6);
-      rect(x + border, y + border, SPOT_WIDTH - 2 * border, height - 2 * border);  
-    }
-    
-    if (!grassy){
-      //lines
-      fill(0);
-      stroke(0);
-      line(x, y, x, y + SPOT_HEIGHT);
-      line(x + SPOT_WIDTH, y, x + SPOT_WIDTH, y + SPOT_HEIGHT);
-    } else {
-      fill("SEAGREEN");
-      rect(x, y - 5, SPOT_WIDTH, SPOT_HEIGHT + 20, 5)
-    }
+  //car
+  if (has_car){
+    fill(color);
+    noStroke();
+    const border = 5;
+    const height = four_door ? SPOT_HEIGHT : SPOT_HEIGHT * (5/6);
+    rect(x + border, y + border, SPOT_WIDTH - 2 * border, height - 2 * border);  
   }
-
-
+  
+  if (!grassy){
+    //lines
+    fill(0);
+    stroke(0);
+    line(x, y, x, y + SPOT_HEIGHT);
+    line(x + SPOT_WIDTH, y, x + SPOT_WIDTH, y + SPOT_HEIGHT);
+  } else {
+    fill("SEAGREEN");
+    rect(x, y - 5, SPOT_WIDTH, SPOT_HEIGHT + 20, 5)
+  }
 }
 
 function getSpotPosition(row, index) {
