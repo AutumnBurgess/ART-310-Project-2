c9a769202a4d3deb99eebcea461140deeafb2b52
AutumnBurgess <aus2001@live.missouristate.edu>
2024-03-07 12:24:06 -0600

    created example data based on google maps very basic drawing based on example data

--- sketch.js
+++ sketch.js
@@ -1,26 +1,60 @@
 let dataTable;
+const SPOT_WIDTH = 40;
+const SPOT_HEIGHT = 60;
+const spotRows = [{x:10, y:10}, {x:10, y:220}];
+let colors = [];
 
 function preload() {
-  dataTable = loadTable("data.csv", "csv", "header");
+  dataTable = loadTable("example_data.csv", "csv", "header");
 }
 
 function setup() {
-  createCanvas(800, 800);
-  colorMode("HSB", 100);
+  createCanvas(1200, 600);
+  textAlign(CENTER);
+
+  
 }
 
 function draw() {
   background(220);
-  ellipse(width / 2, height / 2, 100);
-  for (let rowNumber = 0; rowNumber < dataTable.getRowCount(); rowNumber++) {
-    let row = dataTable.getRow(rowNumber);
-    let x = 100;
-    let y = rowNumber * 100 + 100;
-    let hue = row.getNum("HUE");
-    let c = color(hue, 100, 100);
-    fill(c);
-    ellipse(x, y, row.getNum("SIZE"));
 
-    text(row.getString("NAME"), x + 50, y);
+  for (let dataRow of dataTable.rows){
+    const index = dataRow.getNum("INDEX");
+    const row = dataRow.getNum("ROW");
+    const color = dataRow.getString("COLOR");
+    const has_car = color.length > 0;
+    const grassy = dataRow.getNum("GRASSY") != 0;
+    const four_door = dataRow.getNum("FOUR_DOOR") != 0;
+    const position = getSpotPosition(row, index);
+    const x = position.x;
+    const y = position.y;
+
+    //car
+    if (has_car){
+      fill(color);
+      noStroke();
+      const border = 5;
+      const height = four_door ? SPOT_HEIGHT : SPOT_HEIGHT * (5/6);
+      rect(x + border, y + border, SPOT_WIDTH - 2 * border, height - 2 * border);  
+    }
+    
+    if (!grassy){
+      //lines
+      fill(0);
+      stroke(0);
+      line(x, y, x, y + SPOT_HEIGHT);
+      line(x + SPOT_WIDTH, y, x + SPOT_WIDTH, y + SPOT_HEIGHT);
+    } else {
+      fill("SEAGREEN");
+      rect(x, y - 5, SPOT_WIDTH, SPOT_HEIGHT + 20, 5)
+    }
   }
+
+
+}
+
+function getSpotPosition(row, index) {
+  const x = spotRows[row].x + index * SPOT_WIDTH;
+  const y = spotRows[row].y;
+  return createVector(x, y);
 }
